f7027e2f6c21ce33cb71b563d56f8e6f6ec45d9f
Audrey-L. St-Louis <audreylaurestlouis@gmail.com>
2020-10-23 14:30:55 -0400

    Update Parsing.java

--- src/Parsing.java
+++ src/Parsing.java
@@ -1,78 +1,96 @@
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 public class Parsing {
 	
 	
-	private String methodType;
-	private String host;
-	private String relativePath;
-	private String query;
-	private String key;
-	private String value;
-	private HashMap<String, String> headers = new HashMap();
-	private String formattedRequest;
-	private String readyToSend;
-	private Boolean verbose=false;
-	private String entityBody;
+
+	private String method;
+	private String url;
+	private String version = "HTTP/1.0";
+
 	
 	
-	public void ParseRequest(String request) throws IOException {
+	public static void main(String[] args) throws IOException {
+		Parsing p = new Parsing();
+		p.ParseRequest();
 		
+	}
+	
+	public void ParseRequest() throws IOException {
+		
+		
+		String request = "POST /localhost:8080 HTTP/1.0\r\nContent-length:5\r\nUser-agent:me\r\n\r\n";
+		
+
 		StringReader reader = new StringReader(request);
 		BufferedReader br = new BufferedReader(reader);
-		request = br.readLine();
 	
-
-		// Parse request line
-
-		// 1. Get the methodType of the request
-		methodType = request.substring(0, request.indexOf(" "));
 		
-
-		// 2.Parse headers if there are any
-		while(request.contains("-h")) {
-
-			int h = request.indexOf("-h");
-
-			String temp = request.substring(h);
-
-			h = temp.indexOf("-h");
-			int i = temp.indexOf(":");
-
-			key = temp.substring(h + 2, i);
-			value = temp.substring(i + 1, temp.indexOf(' '));
-			request = temp.substring(temp.indexOf(' '));
-
-			headers.put(key, value);
-
-		}
-
-		// 3. Get the url and split it into host + relative path
-		String str = request.substring(request.indexOf("http"));
-		URL theUrl = new URL(str);
-
-		host = theUrl.getHost();
-		relativePath = theUrl.getPath();
-		if (relativePath == null)
-			relativePath = "";
-
-		// 4. Check if there are any query parameters
-
-		if (request.contains("?")) {
-
-			String temp = request.substring(request.indexOf("?"));
-			query = temp.substring(temp.indexOf("?"), temp.indexOf(' '));
-		} else
-			query = "";
-
+	    HashMap<String, String> headers = new HashMap();
+	    
+	    String initial;
+	    String requestLine[];
+	    
 		
-		
-		
-		
+	    initial = br.readLine();
+	    
+	    
+	    requestLine = initial.split("\\s");
+	
+	    for(int i=0; i<requestLine.length; i++) {
+	    	System.out.println(requestLine[i]);
+	    }
+	    
+	    method = requestLine[0];
+	    
+	    
+	    //headers
+	    String line = br.readLine();
+	    int idx = line.indexOf(":");
+
+
+	    while (!line.equals("")) {
+
+	      idx = line.indexOf(':');
+	      if (idx < 0) {
+	        headers = null;
+	        break;
+	      }
+	      else{
+	    	String key = line.substring(0, idx).toLowerCase();
+			String value = line.substring(idx+1);
+	        headers.put(key, value);
+	      }
+		  line = br.readLine();
+	    }
+	    
+	    for (Entry<String, String> entry : headers.entrySet()) {
+	        System.out.println(entry.getKey() + ":" + entry.getValue().toString());
+	    }
+	    
+	    
+	    // For POST
+	    // Find content-length header
+	    int contentLength;
+	    if(method.equalsIgnoreCase("post"))
+	    {
+	    	contentLength = Integer.parseInt(headers.get("content-length"));	
+	   
+	    }
+	    else
+	    	contentLength=0;
+	
+	    
+	    
+	    
+	    
+
 		
 	}
 
